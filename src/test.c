bf97d33d626bdf865910c381739a73e6ee2fba63
n01e0 <noleo@vanillabeans.mobi>
2019-06-28 09:43:07 +0900

    とりあえずreadmeはこれで一段落

--- src/test.c
+++ src/test.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+int getElement(int i, short array);
+
 void main(){
     int a[5] = {1, 2, 3, 4, 5};
     printf("このint型配列の要素一つのサイズは %d バイト\nこのint型配列全体のサイズは %d バイト\nこのint型配列の3番目の要素は %d\n", sizeof a[2], sizeof a, a[2]);
@@ -8,6 +10,14 @@ void main(){
     short c[5] = {1, 2, 3, 4, 5};
     printf("このshort型配列の要素一つのサイズは %d バイト\nこのshort型配列全体のサイズは %d バイト\nこのshort型配列の3番目の要素は %d\n", sizeof c[2], sizeof c, c[2]);
 
-    unsigned short n = 74565;
-    printf("この数字で表される配列のサイズは %d バイト\nこの配列の要素一つにあたるサイズは 3 ビット\nこの配列の3番目の要素は %d\n",sizeof n,(n >> 8)&15);
+    unsigned short n = 22737;
+    printf("この数字で表される配列のサイズは %d バイト\nこの配列の要素一つにあたるサイズは 3 ビット\nこの配列の3番目の要素は %d\n",sizeof n,(n >> 6)&7);
+
+    printf("%d\n",getElement(n,3));
 }
+
+int getElement(int i, short array){
+    int element = (array >> 4 * 4 * 4 * 4 * (i - 1) & 0b111);
+    return element;
+}
+
