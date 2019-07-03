63016225bbd01a3e64b2dcb715e9890afc855a24
n01e0 <noleo@vanillabeans.mobi>
2019-07-03 19:54:07 +0900

    typo fixed

--- src/setElement.c
+++
@@ -1,7 +0,0 @@
-int setElement(int i, int setNum,short array){
-    int arrayOffset = 3 * (i - 1);
-    int mask = setNum << arrayOffset;
-    int masked (int)pow(2, sizeof(array) - arrayOffset) - 1 << arrayOffset;
-    int newArray = mask & masked;
-    return newArray;
-}
