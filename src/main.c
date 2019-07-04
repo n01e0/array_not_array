d009c69f71c08fd52a73508189ac0bbd8656f244
n01e0 <noleo@vanillabeans.mobi>
2019-07-04 09:22:08 +0900

    fixed bugs and change name

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
-short set(int pt,int setNum, short array);
-int pow(int p, int n);
-int getElement(int i, short array);
+#include "setNum.h"
+#include "getElement.h"
 
 void main(){
     unsigned short array = 22737;
@@ -13,23 +11,4 @@ void main(){
     int m = getElement(3, newArray);
     printf("new array's 3rd element is %d\n", m);
     return 0;
-}
-
-short set(int pt, int setNum, short array){
-    short offset = setNum << 3*(pt - 1);
-    short bottomPadding = pow(2, (3 * setNum - 2));
-    short headPadding = pow(2, (sizeof(array) * (8 - pt * 3) - 1));
-    short newArray = offset + bottomPadding + headPadding;
-}
-
-int pow(int p, int n){
-    int powed = p;
-    for(int i = 0; i < n; i++){
-        powed = powed * p;
-    }
-    return powed;
-}
-int getElement(int i, short array){
-    int element = (array >> 3 * (i - 1) & 0b111);
-    return element;
 }
