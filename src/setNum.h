b8ae90d62c0927536e3c45de756de41cfd8c7821
n01e0 <noleo@vanillabeans.mobi>
2019-07-04 10:06:35 +0900

    include math.h

--- src/setNum.h
+++ src/setNum.h
@@ -1,14 +1,6 @@
 short set(int pt, int setNum, short array){
     short offset = setNum << 3*(pt - 1);
-    short bottomPadding = pow(2, (3 * setNum - 2));
-    short headPadding = pow(2, (sizeof(array) * (8 - pt * 3) - 1));
+    short bottomPadding = (short)pow(2, (float)(3 * setNum - 2));
+    short headPadding = (short)pow(2, (float)(sizeof(array) * (8 - pt * 3) - 1));
     short newArray = offset + bottomPadding + headPadding;
-}
-
-int pow(int p, int n){
-    int powed;
-    for(int i = 0; i < n; i++){
-        powed = p * p;
-    }
-    return powed;
 }
